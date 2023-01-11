d14e1b63123202a2f1a76350dcdd25701b8e9c74
Jose M <35848392+Jnmarti88@users.noreply.github.com>
2023-01-10 22:32:54 -0800

    addition.py had incorrect syntax
    Changed code to correctly ask for 2 numbers , add them and store into sum and print out

--- addition.py
+++ addition.py
@@ -1,6 +1,7 @@
-num1 = int(input("Enter First Number: ")
-num2 = int(input("Enter Second Number: ")
+num1 = int(input("Enter First Number: ")) #asks for First number
+num2 = int(input("Enter Second Number: ")) #ask user for Second number
 
-sum = num1+num2
+sum = num1+num2 #adds First and second number, stores it in sum
 
-print(""Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+print('Sum of {0} and {1} is {2}'.format(num1, num2, sum))
+#print(f'Sum of {num1} and {num2} is {sum}') also works same as above
